d99cffb6c569a34b690ccb175142edd9c3c562aa
Rodion <31254993+nikitinrodi@users.noreply.github.com>
2024-02-01 15:10:33 -0800

    matched example

--- main.tf
+++ main.tf
@@ -47,10 +47,14 @@ resource "aws_instance" "blog" {
   }
 }
 
-module "alb" {
-  source = "terraform-aws-modules/alb/aws"
+module "blog_alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
 
   name            = "blog-alb"
+
+  load_balancer_type = "application"
+
   vpc_id          = module.blog_vpc.vpc_id
   subnets         = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
@@ -62,22 +66,17 @@ module "alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port      = 80
-        }
-      }
     }
   ]
 
-  listeners = {
-    http_tcp_listeners = {
+  http_tcp_listeners = [
+    {
       port               = 80
       protocol           = "HTTP"
       target_group_index = 0
     }
-  }
+  ]
+
 
   tags = {
     Environment = "dev"
